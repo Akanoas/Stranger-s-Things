657de6280137522f36a1948f6ba27e896f474f83
Abdoul Akano <akano1nassirou@gmail.com>
2023-08-16 12:37:16 -0400

    login and register basic setup

--- Strangers-things/src/Pages/Login.jsx
+++ Strangers-things/src/Pages/Login.jsx
@@ -4,7 +4,24 @@ export default function Login({setToken}){
     const [password, setPassword] = useState();
     return(
         <div>
-            Login
+            <h1>Login</h1>
+            <form >
+                <label htmlFor="username">Username</label>
+                <input 
+                type="text"
+                id= 'username'
+                value={username}
+                onChange={(e)=> setUsername(e.target.value)} 
+                />
+                <label htmlFor="password">Password</label>
+                <input 
+                type="password"
+                id = 'password'
+                value= {password}
+                onChange={(e)=> setPassword(e.target.value)} 
+                />
+                <button type="submit">Login</button>
+            </form>
         </div>
     )
 }
